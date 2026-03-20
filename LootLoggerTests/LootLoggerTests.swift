4813562c1934189c6d840ca93aa453168e00631e
RedBeard41 <42767289+RedBeard41@users.noreply.github.com>
2026-03-20 14:29:27 -0400

    Initial Commit

---
+++ LootLoggerTests/LootLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LootLoggerTests.swift
+//  LootLoggerTests
+//
+//  Created by Gabes mac on 3/20/26.
+//
+
+import Testing
+@testable import LootLogger
+
+struct LootLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
